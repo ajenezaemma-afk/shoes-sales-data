64f98c17c520703e1fd1f3861647cd65e8dc5ccd
ajenezaemma-afk <ajenezaemma@gmail.com>
2025-10-18 13:13:04 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,39 +1,31 @@
-
-
-
 import numpy as np
 import streamlit as st
 import pickle
+import os
 
-with open(r"shoes_sales_data.sav", 'rb') as file:
-    model = pickle.load(file)
-    
+# Get path of the model relative to this app.py file
+MODEL_PATH = os.path.join(os.path.dirname(__file__), "shoes_sales_data.sav")
+
 # Load the trained model
-##with open('cancer_patient_prediction_data.sav', 'rb') as file:
-   ## model = pickle.load(file)
+with open(MODEL_PATH, 'rb') as file:
+    model = pickle.load(file)
 
 # Streamlit UI
-st.title("shoes sales data prediction")
+st.title("Shoes Sales Data Prediction")
 st.write("Fill the following information to get a prediction:")
 
 # User Inputs
-brand = st.number_input("brand", value=26)
-color = st.number_input("color", value=4)
-size = st.number_input("size", value=6.5)
-
-
-
+brand = st.number_input("Brand", value=26)
+color = st.number_input("Color", value=4)
+size = st.number_input("Size", value=6.5)
 
 # Prediction on button click
 if st.button("Predict"):
     # Create input in correct order and shape
-    Input = np.array([[brand,color,size]])
+    Input = np.array([[brand, color, size]])
     
     # Predict
     prediction = model.predict(Input)
     
     # Show result
-    st.success(f"shoes sales data prediction is : {prediction[0]:.2f}")
-
-
-
+    st.success(f"Predicted shoe price: {prediction[0]:.2f}")
